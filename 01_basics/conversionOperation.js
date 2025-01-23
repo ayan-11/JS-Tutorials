08e653da58453c397424fd0f14d800be01fd4ee0
Ayan Mukherjee <111264082+ayan-11@users.noreply.github.com>
2025-01-23 17:19:58 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -2,10 +2,10 @@ let score = '33'
 
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 // '33' - 33
@@ -14,9 +14,37 @@ console.log(typeof valueInNumber);
 
 let isLoggedIn = 1
 
-console.log(Boolean(isLoggedIn));
+// console.log(Boolean(isLoggedIn));
 
 
 // 1-true ; 0-false
 //'' - false
 //'ayan'=true
+
+//Operations
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = 'ayan'
+let str3 = str1 +str2
+// console.log(str3);
+ 
+// console.log('1'+2); //output - 12
+// console.log(1+'2'); //output - 12
+// console.log(2 + 2 + '2'); //output - 42
+// console.log('2' +3+2) //output - 232
+
+// console.log(true); //output - true
+// console.log(+true); //output - 1
+// // console.log(true+); //output - gives an error
+// console.log(+''); //output - 0
+
+let num1, num2,num3
+
+let c=100
+c++
+console.log(c);
+
