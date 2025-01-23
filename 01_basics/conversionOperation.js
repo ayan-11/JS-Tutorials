39d7a1fe591dbb12ed9eea136cda9502f191e71b
Ayan Mukherjee <111264082+ayan-11@users.noreply.github.com>
2025-01-23 17:05:47 +0000

    conversion operations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score = '33'
+
+
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+// '33' - 33
+// '33abc' - NaN
+// true - 1; false - 0
+
+let isLoggedIn = 1
+
+console.log(Boolean(isLoggedIn));
+
+
+// 1-true ; 0-false
+//'' - false
+//'ayan'=true
