ded29b298399f66c1809e0d48b8447db9524af0c
Ayan Mukherjee <111264082+ayan-11@users.noreply.github.com>
2025-01-23 16:46:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+// alert(3+3)
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol-unique
+
+// object
+
+console.log(typeof "ayan");
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object
+
