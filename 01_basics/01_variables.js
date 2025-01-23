edb6f78a8779565678ac60a14b9b13787f00f193
Ayan Mukherjee <111264082+ayan-11@users.noreply.github.com>
2025-01-23 16:32:08 +0000

    4th Session

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail="ayan@google.com"
+var accountPassword = "12345"
+accountCity="Jaipur"
+
+
+// accountId = 2
+
+accountEmail="am@hc.com"
+accountPassword='22121212121'
+accountCity = "Bangalore"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
