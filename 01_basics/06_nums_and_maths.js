746473b2eb7beeb4f2c475f2d6bd17a8d49585cb
Ayan Mukherjee <111264082+ayan-11@users.noreply.github.com>
2025-01-24 04:21:14 +0000

    to be continued later

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,9 @@
+const score = 400 
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toFixed(2))
+
+const otherNumber = 25.3695
+console.log((otherNumber.toPrecision(3)));
